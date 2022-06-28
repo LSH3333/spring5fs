94d2d0fd5bae726dcd2298332e523893d91a4e33
LSH3333 <dltpgustpgus@gmail.com>
2022-06-28 15:38:40 +0900

    ch07 CacheAspect

---
+++ sp5-chap07/src/main/java/config/AppCtxWithCache.java
@@ -0,0 +1,34 @@
+package config;
+
+import aspect.CacheAspect;
+import aspect.ExeTimeAspect;
+import chap07.Calculator;
+import chap07.RecCalculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AppCtxWithCache
+{
+    // CacheAspect 먼저
+    @Bean
+    public CacheAspect cacheAspect() {return new CacheAspect();}
+    // ExeAspect 그 다음
+    @Bean
+    public ExeTimeAspect exeTimeAspect()
+    {
+        return new ExeTimeAspect();
+    }
+    
+
+    // ExeTimeAspect 의 measure 메소드가 적용됨
+    @Bean
+    public Calculator calculator()
+    {
+        return new RecCalculator();
+    }
+
+
+}
