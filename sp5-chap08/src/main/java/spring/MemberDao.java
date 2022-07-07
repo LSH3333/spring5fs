add3a9c0989d2ff28d9351dd155063194f890af9
LSH <dltpgustpgus@gmail.com>
2022-07-07 21:58:22 +0900

    MemberDao class에 JdbcTemplate.query()

--- sp5-chap08/src/main/java/spring/MemberDao.java
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -1,10 +1,14 @@
 package spring;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // Member DB
@@ -22,7 +26,28 @@ public class MemberDao
 
     public Member selectByEmail(String email)
     {
-        return null;
+        // List<T> query(String sql, RowMapper<T> rowMapper, Object... args)
+        List<Member> results = jdbcTemplate.query(
+                "select * from MEMBER where EMAIL = ?",
+                // 임의 클래스를 이용해 RowMapper 객체 전달
+                new RowMapper<Member>()
+                {
+                    @Override
+                    public Member mapRow(ResultSet rs, int rowNum) throws SQLException
+                    {
+                        Member member = new Member(
+                                rs.getString("EMAIL"),
+                                rs.getString("PASSWORD"),
+                                rs.getString("NAME"),
+                                rs.getTimestamp("REGDATE").toLocalDateTime());
+                        member.setId(rs.getLong("ID"));
+                        return member;
+                    }
+                }
+                , email
+        );
+
+        return results.isEmpty() ? null : results.get(0);
     }
 
     public void insert(Member member)
